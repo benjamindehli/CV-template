61eb887097285c38475e0c5e0ce3b9ff7be5ec41
Benjamin Dehli <superelg@gmail.com>
2015-10-12 01:58:59 +0200

    Add Vue.js

--- index.php
+++ index.php
@@ -70,6 +70,7 @@
 		</div>
 	</div>
 	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="js/vue.min.js"></script>
 	<script type="text/javascript" src="js/autosize.js"></script>
 	<script type="text/javascript">autosize($('textarea'));</script>
 </body>
